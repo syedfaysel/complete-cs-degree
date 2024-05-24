2d5651cf498f130261157031b19c41199df0458e
Syed Faysel Ahammad Rajo <sfa.rajo20@gmail.com>
2024-05-24 06:36:04 +0600

    google search verification

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,12 +5,19 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Complete CS Degree",
-  description: "Free resources, articles, roadmap, tutorials and many more to learn computer science. A community driven opensource project.",
+  description:
+    "Free resources, articles, roadmap, tutorials and many more to learn computer science. A community driven opensource project.",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <meta
+          name="google-site-verification"
+          content="gBPl8PRPQlE7TZUTQTRJ8O1nLvNc1lso7IqyNYWzrTY"
+        />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
